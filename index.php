66952d25322220132933ecdb2218e263dc128d9b
Abundancia <abundancia@MacBook-Air-de-Abundancia.local>
2023-06-05 09:15:35 +0200

    1er commit Bureau des Etudiants

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4>List de shouates </h4>
+    <ul>
+        <li>On attends le couscous !</li>
+        <li>Améliorer le confort pour tous par des chaises ergonomique, changer les bureau ou mettre a disposition de support pour ordinateur pour qu'ils arrive a hauteur des yeux pour moins se faire mal. Création d'un coin détente avec de pouffe ou canapé pour se reposer ou être en confort. Sortie restaurant 1 jour dans le mois ou la semaine pour se retrouver ou préparation de repas dans nos locaux... Ect..</li>
+        <li>vamos de fiesta~!</li>
+        <li>- Soirée privée sur la plage (voir avec Jules pour ça) - Mettre en place le BIG déjeuner du vendredi matin à la plateforme (une tirelire sera mis en place pour financer tout ça) - Oragniser des sorties à l'extérieur pour la cohésion - Faire livrer des pizza de temps en temps pour bien manger ! (trouver financement et fournisseur acceptant de faire des prix aventageux !) - Créer le site internet du BDE pour proposer pleins de trucs qu'on a pas encore trouvé !!</li>
+        <li>Une idée d'une vision d'une vrai institution.</li>
+        <li>apero pique-nique plage aprés les cour</li>
+    </ul>
+</body>
+</html>
